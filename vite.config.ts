60345a65e372119c0f9e4fccddaade641dd9c011
yusufalp <alpyusuf007@gmail.com>
2026-08-17 21:37:59 -0700

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: [devtools(), netlify(), tailwindcss(), tanstackStart(), viteReact()],
+})
+
+export default config
