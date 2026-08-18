4ae58f5b6825182453c4cfe29c4df35bfeb19221
yusufalp <alpyusuf007@gmail.com>
2026-08-17 23:03:03 -0700

    add format util

---
+++ src/lib/utils/formatCurrency.ts
@@ -0,0 +1,12 @@
+export function formatCurrency(
+  amount: number,
+  currency: string = 'USD',
+  locale: string = 'en-US',
+): string {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(amount)
+}
