80881f9936c6561344bbfac5bb3f2f54d4234974
yusufalp <alpyusuf007@gmail.com>
2026-08-18 22:44:13 -0700

    add routing to home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 import { BudgetSummary } from '#/components/BudgetSummary'
+import { budgets } from '#/data/budgets'
+import { Button } from '#/components/ui/button'
 
 export const Route = createFileRoute('/')({ component: Home })
 
@@ -9,7 +11,15 @@ function Home() {
     <div className="p-8">
       <h1 className="text-4xl font-bold my-4">Welcome to CountSense</h1>
 
-      <BudgetSummary />
+      {budgets.length === 0 ? (
+        <p>No budgets yet.</p>
+      ) : (
+        <BudgetSummary budgets={budgets} />
+      )}
+
+      <Link to={'/budget/new'}>
+        <Button className="mt-4">Create a new budget</Button>
+      </Link>
     </div>
   )
 }
